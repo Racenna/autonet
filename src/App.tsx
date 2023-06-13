c9eb49be5049ddcdae37b6e95f76b4744ab7d4d3
Vlad Kosolapov <vlad.kosolapov48@gmail.com>
2023-06-13 18:38:41 +0300

    [AN-3] created SingIn/up pages and created Error404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routes } from "./const/routes";
+
+const router = createBrowserRouter(routes);
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
